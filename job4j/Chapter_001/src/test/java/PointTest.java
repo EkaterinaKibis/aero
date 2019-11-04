1488d8af0069e627cbddd57571ab2f58e21a7921
ikibis <ilya.kibis@gmail.com>
2019-11-04 21:11:14 +0500

    3. Тесты для расстояние между точками.[#182511]

---
+++ job4j/Chapter_001/src/test/java/PointTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+import ru.kibis.dataTypes.condition.Point;
+
+public class PointTest {
+
+    @Test
+    public void Pointtest() {
+        int expected = 2;
+        int out = (int) Point.distance(1,2,3,4);
+        Assert.assertEquals(expected, out);
+    }
+}
