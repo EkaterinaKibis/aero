eecef4160fff10c2aa6132e57efe9db52fafc98a
ikibis <ilya.kibis@gmail.com>
2019-11-04 21:27:28 +0500

    4.3. А существует ли треугольник?[#182449]

--- job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/condition/Triangle.java
+++ job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/condition/Triangle.java
@@ -3,12 +3,9 @@ package ru.kibis.dataTypes.condition;
 public class Triangle {
     public static boolean exist(double ab, double ac, double bc) {
         boolean q = false;
-        if (((ab + ac) > bc) & ((ab + bc) > ac) && ((ac + bc) > ab)) {
+        if (((ab + ac) > bc) && ((ab + bc) > ac) && ((ac + bc) > ab)) {
             q = true;
         }
-
         return q;
-
     }
 }
-
