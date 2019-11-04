3e06b970a4d16841cf11dfcec9b7a8950c8c5852
ikibis <ilya.kibis@gmail.com>
2019-11-04 21:37:48 +0500

    5.2. Создать программу, вычисляющую факториал.[#182455]

--- job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/loop/Factorial.java
+++ job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/loop/Factorial.java
@@ -3,13 +3,9 @@ package ru.kibis.dataTypes.loop;
 public class Factorial {
     public int calc(int n) {
         int result = 1;
-        int q;
-        if (n == 0) {
-            result = 1;
-        } else
-            for (q = 1; q <= n; q++) {
-                result = result * q;
-            }
+        for (int q = 1; q <= n; q++) {
+            result *= q;
+        }
         return result;
     }
 }
