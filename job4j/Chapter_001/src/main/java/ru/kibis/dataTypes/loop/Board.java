495b3676a729eb4d6f57efe8534b025dbc382840
ikibis <ilya.kibis@gmail.com>
2019-11-04 21:47:51 +0500

    5.3. Построить шахматную доску в псевдографике.[#182458]

--- job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/loop/Board.java
+++ job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/loop/Board.java
@@ -1,19 +1,15 @@
 package ru.kibis.dataTypes.loop;
 
+
 public class Board {
+
     public static void paint(int width, int height) {
-        for ( int i = 0; height >= i; i++) {
-            for ( int g = 0; width >= g; g++) {
-
+        for (int i = 0; height >= i; i++) {
+            for (int g = 0; width >= g; g++) {
                 int sum = i + g;
-                if ((sum % 2) == 0) {
-                    System.out.print("X");
-                } else {
-                    System.out.print(" ");
-                }
+                String result = (sum % 2 == 0) ? "X" : " ";
+                System.out.println(result);
             }
-
-            System.out.println();
         }
     }
 
