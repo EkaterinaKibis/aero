b13d0905791fff67eadbd0ee8ec505ae2a9d7e42
ikibis <ilya.kibis@gmail.com>
2019-11-04 22:32:32 +0500

    5.6. Простые числа.[#182460]

--- job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/loop/PrimeNumber.java
+++ job4j/Chapter_001/src/main/java/ru/kibis/dataTypes/loop/PrimeNumber.java
@@ -3,18 +3,12 @@ package ru.kibis.dataTypes.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
+        CheckPrimeNumber checker = new CheckPrimeNumber();
         for (int num = 2; num <= finish; num++) {
-            boolean prime = true;
-            for (int i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    prime = false;
-                }
-            }
-            if (prime) {
+            if (checker.check(num)) {
                 count++;
             }
         }
         return count;
     }
 }
-
