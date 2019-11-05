dbca88bc75a700ddefc4f1eda48456b77b904990
ikibis <ilya.kibis@gmail.com>
2019-11-05 20:57:55 +0500

    6.4. Слово начинается с ...[#182468]

---
+++ job4j/Chapter_001/src/test/java/arrayTest/ArrayCharTest.java
@@ -0,0 +1,25 @@
+package arrayTest;
+
+
+import org.junit.Test;
+import ru.kibis.dataTypes.array.ArrayChar;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayCharTest {
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] pref = {'H', 'f'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(false));
+    }
+}
